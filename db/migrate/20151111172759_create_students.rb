5016785328cd9070a4fa0fa0fba161b713a7d1cf
Yebin Han <yh117@duke.edu>
2015-11-11 17:29:48 +0000

    created students table migration file

---
+++ db/migrate/20151111172759_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string   :email
+      t.integer  :course_id
+      t.string   :random_url
+      t.integer  :student_user_id
+      t.timestamps
+    end
+  end
+end
